350709c8059d0c89f291c2d75da7d87a9ca94fd5
edcastillob <edwar.castillo@gmail.com>
2024-01-05 12:41:42 -0400

    Modelo reviews

---
+++ src/models/Review.js
@@ -0,0 +1,36 @@
+// models/Review.js
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Review = sequelize.define(
+    'Review',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      comment: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      productId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    { timestamps: true }
+  );
+
+  return Review;
+};
